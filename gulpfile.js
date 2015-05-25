fb46ae9a7df80ac7d24b3dfab52d4fd4eb1c5d56
Daniel Waghorn <daniel@daniel-waghorn.co.uk>
2015-05-25 11:39:13 +0100

    Added Gulpfile
    Added gulpfile and changed jquery.slimmenu.js to not output menu
    collapser automatically so I could add it manually. Event handler is
    left intact to retain desired functionality.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// Gulpfile for minifying repository.
+
+var gulp = require('gulp');
+// var coffee = require('gulp-coffee'); 
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('default', ['js', 'css']);
+
+gulp.task('js', function(){
+	return gulp.src('./js/src/*.js')
+	.pipe(uglify()) 
+	.pipe(rename({extname: '.min.js'}))
+	.pipe(gulp.dest('./js/'));
+});
+
+gulp.task('css', function(){
+	return gulp.src('./css/src/*.css')
+	.pipe(autoprefixer('last 2 versions')) 
+	.pipe(gulp.dest('./css/')) 
+	.pipe(minifyCss()) 
+	.pipe(rename({extname: '.min.css'})) 
+	.pipe(gulp.dest('./css/'));
+});
